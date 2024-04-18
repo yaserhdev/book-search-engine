723121d5dc178da2523f407eecb2560038c850ab
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2024-04-17 22:56:25 -0400

    Refactored server.js to use an Apollo server with GraphQL

--- server/server.js
+++ server/server.js
@@ -1,21 +1,46 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
+// Import ApolloServer class and expressMiddleware helper function
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+
+// Import two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./models');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+// Define server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Create new instance of an Apollo server with GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server));
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`User GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.use(routes);
+// app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Start server
+startApolloServer();
