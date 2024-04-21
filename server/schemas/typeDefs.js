38159f85f6dae6e81b065598c2084e6a3e0d3e54
Raja Yaser Hussain <yaser@Yasers-MBP.fios-router.home>
2024-04-21 16:34:34 -0400

    Updated typeDefs and added mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,15 +2,16 @@
 const typeDefs = `
 # Define which fields are accessible from the User model
 type User {
+    _id: ID
     username: String
     email: String
     password: String
-    savedBooks: [books]
+    savedBooks: [Book]!
 }
 
 # Define which fields are accessible from the Book model
 type Book {
-    authors: [{String}]
+    authors: [String]
     description: String
     bookId: String
     image: String
@@ -21,7 +22,18 @@ type Book {
 # Define which queries the front end is allowed to make and what data is returned
 type Query {
     users: [User]
-    books: [Book]
+    user(username: String!): User
+    books(username: String!): [Book]
+    // book(bookId: ID!): Book
+    me: User
+}
+
+# Define which mutations can be made by a user
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookId: ID!, title: String!, authors: [String], description: String, image: String, link: String): User
+    removeBook(bookId: ID!): User
 }
 `;
 
