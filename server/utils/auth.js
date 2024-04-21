766fee0ffba69535678261c5854f2360f44a5d8d
Raja Yaser Hussain <yaser@Yasers-MBP.fios-router.home>
2024-04-21 17:05:50 -0400

    Refactored auth file to work with GraphQL

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,4 @@
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -5,10 +6,17 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    }
+  }),
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  // authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,24 +24,28 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      // return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+      // const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      const { authenticatedPerson } = jwt.verify(token, secret, { maxAge: expiration });
+      // req.user = data;
+      req.user = authenticatedPerson;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      // return res.status(400).json({ message: 'invalid token!' });
     }
-
+    return req;
     // send to next endpoint
-    next();
+    // next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    // return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return jwt.sign({ authenticatedPerson: payload }, secret, { expiresIn: expiration });
   },
 };
