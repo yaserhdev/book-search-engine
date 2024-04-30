a1ce21445834457e116e0dc8d6d37c19c2ff158f
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2024-04-29 22:39:26 -0400

    Changed saved book population

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -49,7 +49,7 @@ const SavedBooks = () => {
       </div>
       <Container>
         <h2 className='pt-5'>
-          {userData.savedBooks.length
+          {!loading
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
